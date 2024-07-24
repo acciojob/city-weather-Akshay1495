c334a179c2dd4ddb79afe9ba3d2394478683e3fb
Akshay <8009akshy@gmail.com>
2024-07-24 08:22:20 +0530

    try1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
+const fruits = ["apple","banana","cherry","date","elderberry","fig"];
+
 const App = () => {
+  const[value, setValue] = useState("");
+  const fillteredFruits = fruits.filter((fruit) =>
+  fruit.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input
+        type ="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)} 
+      />
+      <ul>
+          {fillteredFruits.map((fruit, i) => (
+            <li key={i}>{fruit}</li>
+          ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
