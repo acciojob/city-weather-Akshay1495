7922526aff39ac8bc2bf1f39f319cc8aaaacc7ec
Akshay <8009akshy@gmail.com>
2024-07-24 08:35:26 +0530

    test1

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,60 @@
+import "@babel/polyfill";
 
-import React, {useState} from "react";
+import React,{useState,useEffect } from "react";
 import './../styles/App.css';
 
-const fruits = ["apple","banana","cherry","date","elderberry","fig"];
-
 const App = () => {
-  const[value, setValue] = useState("");
-  const fillteredFruits = fruits.filter((fruit) =>
-  fruit.toLocaleLowerCase().includes(value.toLocaleLowerCase())
-);
-  return (
-    <div>
-      <input
-        type ="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)} 
-      />
-      <ul>
-          {fillteredFruits.map((fruit, i) => (
-            <li key={i}>{fruit}</li>
-          ))}
-      </ul>
-    </div>
-  );
-};
+  const [city, setCity] = useState("");
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+  const [error, setError] = useState("");
 
-export default App;
+  useEffect(() => {
+  const fetchApi = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=2d19741fec5e1ebbce4e4b02007b3734`;
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          setError('City not found');
+          throw new Error('City not found');
+          
+        }
+        setError('')
+        return response.json();
+      })
+      .then(data => setWeather(data))
+      .catch(error => setWeather({error: error.message}));
+  }
+  fetchApi();
+}, [city]);
+
+  
+  function display(){
+    setCity(search)
+  }
+   
+  return (
+    <div className="main" id="main">
+      <div className="search">
+        <input type="text" placeholder="Enter a city" onChange={(e)=>setSearch(e.target.value)}/>
+        <button onClick={display}>Search</button>
+      </div>
+      <div className="weather">
+        { error && 
+          <h2>{error}</h2>
+        }
+        { weather.main && 
+          <>
+            <h1>{city}</h1>
+            <h2>{weather.main.temp}°Cel</h2>
+            <h3>{weather.weather[0].description}</h3>
+            <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt="cloud image"/>
+          </>
+        }
+      </div> 
+    </div>
+  )
+  
+}
+
+export default App
